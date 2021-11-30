1a9b18a3bc12f6441c779afa4b08b1a972f14270
Victor GALLOIS-WONG <vgallois@e2r1p6.clusters.42paris.fr>
2021-11-30 19:48:14 +0100

    cmd_part_one norme et fix cd

--- srcs/exec/cmd_part_one.c
+++ srcs/exec/cmd_part_one.c
@@ -6,185 +6,184 @@
 /*   By: bbaudry <bbaudry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 13:34:31 by bbaudry           #+#    #+#             */
-/*   Updated: 2021/11/30 17:08:03 by bbaudry          ###   ########.fr       */
+/*   Updated: 2021/11/30 19:45:58 by vgallois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int ft_echo(t_struct lst, char **cmd_parts, char ***env)
+int	ft_echo(t_struct lst, char **cmd_parts, char ***env)
 {
-    int x;
-    int n;
+	int		x;
+	int		n;
 
-    (void)env;
+	(void)env;
 	n = 1;
-    if ((cmd_parts[1]))
-    {
-	    x = 1;
-	    n = ft_strncmp(cmd_parts[1], "-n", 2);
-	    if (n == 0)
-	        x = 2;
-	    while (cmd_parts[x])
-	    {
-	        ft_putstr_fd(cmd_parts[x++], 1);
-	        if (cmd_parts[x])
-	          write(1, " ", 1);
-	    }
+	if ((cmd_parts[1]))
+	{
+		x = 1;
+		n = ft_strncmp(cmd_parts[1], "-n", 2);
+		if (n == 0)
+			x = 2;
+		while (cmd_parts[x])
+		{
+			ft_putstr_fd(cmd_parts[x++], 1);
+			if (cmd_parts[x])
+				write(1, " ", 1);
+		}
 	}
-    if (n != 0)
-        write(1, "\n", 1);
-    if (lst.is_child)
-        exit (EXIT_SUCCESS);
-    return (0);
+	if (n != 0)
+		write(1, "\n", 1);
+	if (lst.is_child)
+		exit (EXIT_SUCCESS);
+	return (0);
 }
 
-int cd_base(char *buf)
+int	cd_base(t_struct lst)
 {
-    char *new;
-    int x;
-    int y;
+	char	*new;
+	int		ret;
 
-    x = 0;
-    y = 0;
-    while (y < 3)
-    if (buf[x++] == '/')
-      y++;
-    y = -1;
-    if (!(new = malloc((x - 1) * sizeof(char))))
-        return (error(MEM_ERR, NULL, NULL, 0));
-    while (++y < x)
-    new[y] = buf[y];
-    chdir(new);
-    free(new);
-    return (0);
-	//exit(EXIT_SUCCESS);
+	new = ft_get_env("HOME", *(lst.env));
+	if (new)
+		ret = chdir(new);
+	else
+		ret = chdir("/");
+	return (ret);
 }
 
-int cd_relative(char **cmd_parts, char *buf, size_t len)
+int	cd_relative(char **cmd_parts, char *buf, size_t len)
 {
-    char *target;
-    int x;
-    int y;
+	char	*target;
+	int		x;
+	int		y;
 
-    if (!(target = malloc(len + ft_strlen(cmd_parts[1]))))
+	target = malloc(len + ft_strlen(cmd_parts[1]));
+	if (!target)
 		return (error(MEM_ERR, NULL, NULL, 0));
-    x = 0;
-    while (buf[x])
-    {
-        target[x] = buf[x];
-        x++;
-    }
-    y = 0;
-    while (cmd_parts[1][y])
-    {
-        target[x + y + 1] = cmd_parts[1][y];
-        y++;
-    }
-    target[x + y] = '\0';
-    if (chdir(target) == -1)
+	x = 0;
+	while (buf[x])
+	{
+		target[x] = buf[x];
+		x++;
+	}
+	y = 0;
+	while (cmd_parts[1][y])
+	{
+		target[x + y + 1] = cmd_parts[1][y];
+		y++;
+	}
+	target[x + y] = '\0';
+	if (chdir(target) == -1)
 		return (error(MEM_ERR, NULL, NULL, 0));
 	return (0);
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
 
-int ft_cd(t_struct lst, char **cmd_parts, char ***env)
+static int	cd_part2(t_struct lst, char **cmd_parts, char *buf, size_t len)
 {
-    char *buf;
-    size_t len;
-
-    (void)env;
-    len = 1;
-    if (!(buf = malloc(len * sizeof(char))))
+	if (cmd_parts[1] == NULL)
+	{
+		if (cd_base(lst))
+			return (error(MEM_ERR, NULL, NULL, 0));
+	}
+	else if (cmd_parts[2] != NULL)
 		return (error(MEM_ERR, NULL, NULL, 0));
-    while (getcwd(buf, len) == NULL)
-    {
-        free(buf);
-        len++;
-        if (!(buf = malloc(len * sizeof(char))))
-			return (error(MEM_ERR, NULL, NULL, 0));
-    }
-    if (cmd_parts[1] == NULL)
-    {
-        if (cd_base(buf) != 1)
-			return (error(MEM_ERR, NULL, NULL, 0));
-    }
-    else if (cmd_parts[2] != NULL)
+	else if ((chdir(cmd_parts[1]) == -1)
+		&& (cd_relative(cmd_parts, buf, len) == -1))
+	{
+		printf("%s : no such file or directory\n", cmd_parts[1]);
+		free(buf);
 		return (error(MEM_ERR, NULL, NULL, 0));
-    else if (chdir(cmd_parts[1]) == -1)
-        if (cd_relative(cmd_parts, buf, len) == -1)
-        {
-            printf("%s : no such file or directory\n", cmd_parts[1]);
-            free(buf);
-			return (error(MEM_ERR, NULL, NULL, 0));
-        }
-    free(buf);
-    if (lst.is_child)
-        exit (EXIT_SUCCESS);
-    return (0);
-
+	}
+	free(buf);
+	if (lst.is_child)
+		exit(0);
+	return (0);
 }
 
-char *ft_pwd_in()
+int	ft_cd(t_struct lst, char **cmd_parts, char ***env)
 {
-    char *buf;
-    size_t len;
+	char	*buf;
+	size_t	len;
 
-    len = 0;
-    if (!(buf = malloc(len * sizeof(char))))
+	(void)env;
+	len = 1;
+	buf = malloc(len * sizeof(char));
+	if (!buf)
+		return (error(MEM_ERR, NULL, NULL, 0));
+	while (getcwd(buf, len) == NULL)
+	{
+		free(buf);
+		len++;
+		buf = malloc(len * sizeof(char));
+		if (!buf)
+			return (error(MEM_ERR, NULL, NULL, 0));
+	}
+	return (cd_part2(lst, cmd_parts, buf, len));
+}
+
+char	*ft_pwd_in(void)
+{
+	char	*buf;
+	size_t	len;
+
+	len = 0;
+	buf = malloc(len * sizeof(char));
+	if (!buf)
 		return (NULL);
-    while (getcwd(buf, len) == NULL)
-    {
-        free(buf);
-        len++;
-        if (!(buf = malloc(len * sizeof(char))))
+	while (getcwd(buf, len) == NULL)
+	{
+		free(buf);
+		len++;
+		buf = malloc(len * sizeof(char));
+		if (!buf)
 			return (NULL);
-    }
-    // ft_putstr_fd(buf, 1);
-    // write(1, "\n", 1);
-    // free(buf);
-    return (buf);
+	}
+	return (buf);
 }
 
-int ft_pwd(t_struct lst, char **cmd_parts, char ***env)
+int	ft_pwd(t_struct lst, char **cmd_parts, char ***env)
 {
-    char *buf;
-    size_t len;
+	char	*buf;
+	size_t	len;
 
-    (void)env;
-    (void)cmd_parts;
-    len = 0;
-    if (!(buf = malloc(len * sizeof(char))))
-		return (error(MEM_ERR, NULL, NULL, 0));
-    while (getcwd(buf, len) == NULL)
-    {
-        free(buf);
-        len++;
-        if (!(buf = malloc(len * sizeof(char))))
-			return (error(MEM_ERR, NULL, NULL, 0));
-    }
-    ft_putstr_fd(buf, 1);
-    write(1, "\n", 1);
-    free(buf);
-    if (lst.is_child)
-        exit (EXIT_SUCCESS);
-    return (0);
-}
-
-int ft_env(t_struct lst, char **cmd_parts, char ***env)
-{
-    int i;
+	(void)env;
 	(void)cmd_parts;
-
-    i = 0;
-    while ((*env)[i])
-        printf("%s\n", (*env)[i++]);
-    if (lst.is_child)
-        exit (EXIT_SUCCESS);
-    return (0);
+	len = 0;
+	buf = malloc(len * sizeof(char));
+	if (!buf)
+		return (error(MEM_ERR, NULL, NULL, 0));
+	while (getcwd(buf, len) == NULL)
+	{
+		free(buf);
+		len++;
+		buf = malloc(len * sizeof(char));
+		if (!buf)
+			return (error(MEM_ERR, NULL, NULL, 0));
+	}
+	ft_putstr_fd(buf, 1);
+	write(1, "\n", 1);
+	free(buf);
+	if (lst.is_child)
+		exit (EXIT_SUCCESS);
+	return (0);
 }
 
-int ft_export(t_struct lst, char **cmd_parts, char ***env)
+int	ft_env(t_struct lst, char **cmd_parts, char ***env)
+{
+	int	i;
+
+	(void)cmd_parts;
+	i = 0;
+	while ((*env)[i])
+		printf("%s\n", (*env)[i++]);
+	if (lst.is_child)
+		exit (EXIT_SUCCESS);
+	return (0);
+}
+
+int	ft_export(t_struct lst, char **cmd_parts, char ***env)
 {
 	int	i;
 
@@ -194,13 +193,13 @@ int ft_export(t_struct lst, char **cmd_parts, char ***env)
 		ft_setenv(env, cmd_parts[i]);
 		i++;
 	}
-    ft_env(lst , cmd_parts, env);
-    if (lst.is_child)
-        exit (EXIT_SUCCESS);
-    return (0);
+	ft_env(lst, cmd_parts, env);
+	if (lst.is_child)
+		exit (EXIT_SUCCESS);
+	return (0);
 }
 
-int ft_unset(t_struct lst, char **cmd_parts, char ***env)
+int	ft_unset(t_struct lst, char **cmd_parts, char ***env)
 {
 	int	i;
 
@@ -210,17 +209,17 @@ int ft_unset(t_struct lst, char **cmd_parts, char ***env)
 		ft_unsetenv(env, cmd_parts[i]);
 		i++;
 	}
-    if (lst.is_child)
-        exit (EXIT_SUCCESS);
-    return (0);
+	if (lst.is_child)
+		exit (EXIT_SUCCESS);
+	return (0);
 }
 
-int ft_exit(t_struct lst, char **cmd_parts, char ***env)
+int	ft_exit(t_struct lst, char **cmd_parts, char ***env)
 {
-    (void)lst;
-    ft_free(cmd_parts);
-    ft_free(*env);
-    rl_clear_history();
-    printf("Bye bye\n");
-    exit(EXIT_SUCCESS);
+	(void)lst;
+	ft_free(cmd_parts);
+	ft_free(*env);
+	rl_clear_history();
+	printf("Bye bye\n");
+	exit(EXIT_SUCCESS);
 }
